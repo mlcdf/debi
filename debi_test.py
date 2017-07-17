6c579155ceeab872d57ffb7445bfecc16a7c2dc2
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-07-17 15:33:12 +0200

    test: clean code

--- debi_test.py
+++ debi_test.py
@@ -1,13 +1,17 @@
+"""Tests for debi. Run via `pytest`"""
+# pylint: disable=C0111
+
 import delegator
 
 
-class TestDebi:
-    def test_package_64bits(self):
-        assert delegator.run('debi atom atom').return_code == 0
+def test_package_64bits():
+    assert delegator.run('debi atom atom').return_code == 0
 
-    def test_package_64bits_beta(self):
-        assert delegator.run('debi atom atom --beta').return_code == 0
 
-    def test_package_32bits(self):
-        assert delegator.run(
-            'debi webtorrent webtorrent-desktop').return_code == 0
+def test_package_64bits_beta():
+    assert delegator.run('debi atom atom --beta').return_code == 0
+
+
+def test_package_32bits():
+    assert delegator.run(
+        'debi webtorrent webtorrent-desktop').return_code == 0
