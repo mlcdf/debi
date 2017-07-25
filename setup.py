f102e602499ff250f085dc065015d1dbeeedc008
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-07-26 00:19:31 +0200

    pypi: add long description

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import codecs
+import os
+
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(os.path.join(here, "__version__.py")) as f:
+    exec(f.read(), about)
 
 setup(
     name='debi',
-    version='0.2.0',
+    version=about['__version__'],
     author='Maxime Le Conte des Floris',
     author_email='hello@mlcdf.com',
     description='Command-line interface for installing Debian packages via GitHub releases',
+    long_description=long_description,
     py_modules=['debi'],
     install_requires=['Click', 'requests', 'logsymbols'],
-    python_requires='>=3',
     entry_points='''
         [console_scripts]
         debi=debi:cli
